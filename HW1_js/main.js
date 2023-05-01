4bcd5c7c49ed4b7d0aff6190767e3c99a7c189ab
leruisi <yuristovskaya7@gmail.com>
2023-05-01 23:42:32 +0300

    Add file index.html main.js Done homework 1 (task 1, 2, 3)

---
+++ HW1_js/main.js
@@ -0,0 +1,48 @@
+// 1
+let a = 'hello';
+let b = "owu"
+let c = "com"
+let d = "ua"
+let e = 1
+let f = 10
+let g = 123;
+let h = 3.14
+let i = 2.7;
+let j = 16
+let k = true;
+let l = false
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+
+
+//2
+let firstName = 'Valeriia';
+let middleName = 'Maryanivna';
+let lastName = 'Yurisrovska';
+
+let user =`${firstName} ${middleName} ${lastName} ` ;
+
+console.log(user);
+
+
+//3
+
+
+let a1 = 100;
+let b2 = '100';
+let c3 = true;
+
+console.log(typeof a1);
+console.log(typeof b2);
+console.log(typeof c3);
